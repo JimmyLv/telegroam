3c7bb7ad095ff898c70547443cb9eb8fee0e42fb
JimmyLv <jimmy.jinglv@gmail.com>
2022-09-24 20:31:36 +0800

    feat: matched URL in the text

--- src/telegroam.js
+++ src/telegroam.js
@@ -439,6 +439,16 @@
 
         debug("========disabledDatePrefix========", disabledDatePrefix);
         const datePrefix = disabledDatePrefix === "true" ? "" : `${hhmm} `;
+
+        const urlRE = new RegExp(
+          "([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?([^ ])+",
+          "g"
+        );
+        const matched = text.match(urlRE);
+        if (matched) {
+          console.log("========text contains URL========", matched);
+        }
+
         createNestedBlock(parent, {
           uid,
           order: maxOrder + i,
